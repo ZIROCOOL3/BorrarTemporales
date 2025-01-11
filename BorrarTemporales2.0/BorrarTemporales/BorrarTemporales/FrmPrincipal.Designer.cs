d017a1769997b9dcfb7ff26aa63d49e5f762f8c2
JAVIER CANTEROS <ZIROCOOL2003@HOTMAIL.COM>
2025-01-10 22:35:03 -0300

    10-01-25
    version 2.0

---
+++ BorrarTemporales2.0/BorrarTemporales/BorrarTemporales/FrmPrincipal.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace BorrarTemporales
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.PgbBorrar = new Telerik.WinControls.UI.RadProgressBar();
+            this.HiloBorrar = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.PgbBorrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PgbBorrar
+            // 
+            this.PgbBorrar.Location = new System.Drawing.Point(12, 12);
+            this.PgbBorrar.Name = "PgbBorrar";
+            this.PgbBorrar.Size = new System.Drawing.Size(308, 44);
+            this.PgbBorrar.TabIndex = 0;
+            // 
+            // HiloBorrar
+            // 
+            this.HiloBorrar.WorkerReportsProgress = true;
+            this.HiloBorrar.DoWork += new System.ComponentModel.DoWorkEventHandler(this.HiloBorrar_DoWork);
+            this.HiloBorrar.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.HiloBorrar_ProgressChanged);
+            this.HiloBorrar.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.HiloBorrar_RunWorkerCompleted);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 68);
+            this.Controls.Add(this.PgbBorrar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Borrar Temporales";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PgbBorrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadProgressBar PgbBorrar;
+        private System.ComponentModel.BackgroundWorker HiloBorrar;
+    }
+}
---
+++ BorrarTemporales2.0/BorrarTemporales/BorrarTemporales/FrmPrincipal.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace BorrarTemporales
+{
+    public partial class FrmPrincipal : Telerik.WinControls.UI.RadForm
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void radProgressBar1_Click(object sender, EventArgs e)
+        {
+                    }
+
+        private void HiloBorrar_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string path = @"C:\Users\JAVI\AppData\Local\Temp";
+            int Total = 0;
+            int i = 0;
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            //cuento cuantos archivos tiene
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                Total++;
+            }
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+            {
+                Total++;
+            }
+
+            PgbBorrar.Maximum = Total;
+
+
+            //borro archivos
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                i++;
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception)
+                {
+
+
+                }
+
+                HiloBorrar.ReportProgress(i);
+
+            }
+            //borro carpetas
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+            {
+                i++;
+                try
+                {
+                    dir.Delete(true);
+                }
+                catch (Exception)
+                {
+
+
+                }
+                HiloBorrar.ReportProgress(i);
+
+            }
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            HiloBorrar.RunWorkerAsync();
+        }
+
+        private void HiloBorrar_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PgbBorrar.Value1 = e.ProgressPercentage;
+        }
+
+        private void HiloBorrar_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
